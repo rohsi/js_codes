fa332dcfbd2cc0d001146546819543e295d3685e
Mohit Sinha <mohitsinha251@gmail.com>
2019-01-03 17:20:41 +0700

    Create multi_array_student_q2.js

---
+++ loops_filter_for/multi_array_student_q2.js
@@ -0,0 +1,53 @@
+var students = [
+    {
+        rollNo: 1,
+        name: "Amar",
+        class: 6
+    },
+    {
+        rollNo: 2,
+        name: "Joe",
+        class: 9
+    }
+]
+
+var results = [
+    {
+        rollNo: 1,
+        marks: 43,
+        subject: 'Science'
+    },
+    {
+        rollNo: 1,
+        marks: 37,
+        subject: 'Maths'
+    }, {
+        rollNo: 1,
+        marks: 47,
+        subject: 'English'
+    },
+    {
+        rollNo: 2,
+        marks: 37,
+        subject: 'Maths'
+    },
+]
+
+var sports = [
+    {
+        studentId: 1,
+        activity: "football"
+    }, {
+        studentId: 1,
+        activity: "cricket"
+    }, {
+        studentId: 1,
+        activity: "chess"
+    }, {
+        studentId: 2,
+        activity: "gymnastic"
+    }, {
+        studentId: 2,
+        activity: "dance"
+    },
+]
